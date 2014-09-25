349d20589543e69652adc6c56723ea3e94e4b20e
Sebastian Erdweg <seba.github@gmail.com>
2014-09-25 09:19:46 +0200

    rename synthesizer modules

--- src/org/sugarj/common/cleardep/Synthesizer.java
+++ src/org/sugarj/common/cleardep/Synthesizer.java
@@ -11,25 +11,25 @@ import org.sugarj.common.path.Path;
  * @author Sebastian Erdweg
  */
 public class Synthesizer {
-  public Set<CompilationUnit> modules;
+  public Set<CompilationUnit> generatorModules;
   public Map<Path, Integer> files;
   
   public Synthesizer(Set<CompilationUnit> modules, Map<Path, Integer> files) {
-    this.modules = modules;
+    this.generatorModules = modules;
     this.files = files;
   }
 
   public Synthesizer(Stamper stamper, Set<CompilationUnit> modules, Set<Path> files) {
-    this.modules = modules;
+    this.generatorModules = modules;
     this.files = new HashMap<>();
     for (Path p : files)
       this.files.put(p, stamper.stampOf(p));
   }
 
-  public void markSynthesized(CompilationUnit c) {
-    for (CompilationUnit m : modules)
-      c.addModuleDependency(m);
+  public void markSynthesized(CompilationUnit synthesizedModule) {
+    for (CompilationUnit m : generatorModules)
+      synthesizedModule.addModuleDependency(m);
     for (Entry<Path, Integer> e : files.entrySet())
-      c.addExternalFileDependency(e.getKey(), e.getValue());
+      synthesizedModule.addExternalFileDependency(e.getKey(), e.getValue());
   }
 }
