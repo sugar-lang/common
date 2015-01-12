492f106b9a38f25ab0aa23a30b27caf5355d1326
Sebastian Erdweg <seba.github@gmail.com>
2015-01-12 11:12:33 +0100

    organize imports

--- src/org/sugarj/common/cleardep/BuildSchedule.java
+++ src/org/sugarj/common/cleardep/BuildSchedule.java
@@ -1,8 +1,6 @@
 package org.sugarj.common.cleardep;
 
-import java.util.Deque;
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
