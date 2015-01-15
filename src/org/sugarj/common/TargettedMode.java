3a01c004577a705183b13dea41f9e65eba5a90fa
Sebastian Erdweg <seba.github@gmail.com>
2015-01-15 17:20:10 +0100

    make Mode interface

--- src/org/sugarj/common/TargettedMode.java
+++ src/org/sugarj/common/TargettedMode.java
@@ -4,9 +4,7 @@ import org.sugarj.common.cleardep.CompilationUnit;
 import org.sugarj.common.cleardep.Mode;
 import org.sugarj.common.path.Path;
 
-public abstract class TargettedMode<E extends CompilationUnit> extends Mode<E> {
-  private static final long serialVersionUID = -8134431329009920326L;
-
+public interface TargettedMode<E extends CompilationUnit> extends Mode<E> {
   public abstract Path getTargetDir();
   public abstract boolean isTemporary();
 }
--- src/org/sugarj/common/cleardep/Mode.java
+++ src/org/sugarj/common/cleardep/Mode.java
@@ -3,30 +3,8 @@ package org.sugarj.common.cleardep;
 import java.io.Serializable;
 
 
-public abstract class Mode<E extends CompilationUnit> implements Serializable {
-  
-  private static final long serialVersionUID = -5374901699571451411L;
-
-//  private Mode<E> parent;
-//  
-//  public Mode(Mode<E> parent) {
-//    this.parent = parent;
-//  }
-  
-  public abstract Mode<E> getModeForRequiredModules();
-  public abstract boolean canAccept(E e);
-  public abstract E accept(E e);
-  
-//  protected Mode<E> getParentsmodeForRequiredModules() {
-//    return parent == null ? null : parent.getModeForRequiredModules();
-//  }
-//  
-//  @SuppressWarnings("unchecked")
-//  protected static <M extends Mode<?>> M getModeAs(Mode<?> mode, Class<M> cl) {
-//    if (cl.isInstance(mode))
-//      return (M) mode;
-//    if (mode.parent != null)
-//      return getModeAs(mode.parent, cl);
-//    return null;
-//  }
+public interface Mode<E extends CompilationUnit> extends Serializable {
+  public Mode<E> getModeForRequiredModules();
+  public boolean canAccept(E e);
+  public E accept(E e);
 }
