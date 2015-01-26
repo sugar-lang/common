92b9159b529664ded355579020e41e01585bef4d
Moritz Lichter <lichtemo@googlemail.com>
2015-01-26 13:07:09 +0100

    Removed cleardep files

--- src/org/sugarj/common/cleardep/DependencyExtractor.java
+++
@@ -1,9 +0,0 @@
-package org.sugarj.common.cleardep;
-
-import java.util.Set;
-
-public interface DependencyExtractor {
-  
-  public Set<CompilationUnit> extractDependencies(CompilationUnit unit);
-
-}
