a6f75ead262e4b6678776a85914c424604c8c21b
Sebastian Erdweg <seba.github@gmail.com>
2015-02-24 16:04:21 +0100

    array utils

---
+++ src/org/sugarj/common/util/ArrayUtils.java
@@ -0,0 +1,29 @@
+package org.sugarj.common.util;
+
+import java.lang.reflect.Array;
+
+public class ArrayUtils {
+  public static <T> T[] arrayConcat(T[] ar1, T[] ar2) {
+    if (ar1 == null)
+      return ar2;
+    if (ar2 == null)
+      return ar1;
+    
+  	@SuppressWarnings("unchecked")
+  	T[] ar = (T[]) Array.newInstance(ar1.getClass().getComponentType(), ar1.length + ar2.length);
+  	
+  	System.arraycopy(ar1, 0, ar, 0, ar1.length);
+  	System.arraycopy(ar2, 0, ar, ar1.length, ar2.length);
+  	return ar;
+  }
+  
+  public static <T> T[] arrayAdd(T t, T[] ar) {
+  	@SuppressWarnings("unchecked")
+  	T[] arRes = (T[]) Array.newInstance(ar.getClass().getComponentType(), ar.length + 1);
+  	arRes[0] = t;
+  	System.arraycopy(ar, 0, arRes, 1, ar.length);
+  	return arRes;
+  }
+  
+}
+
